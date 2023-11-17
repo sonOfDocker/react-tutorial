4b6a3873945e4456648f8c71c0c76b7cef6b4e2b
Abraham Leal <abraham.carlos.leal@pm.me>
2023-11-17 17:00:15 -0500

    Add multiple boxes using fragment

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+    <>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+  </>
+  );
 }
+
